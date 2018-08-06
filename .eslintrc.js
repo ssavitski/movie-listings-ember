e404d3d64fc3abe16816ce13de3dccb9afbbbe6b
Siarhei Savitski <s.savitski@myxtramile.com>
2018-08-06 13:55:27 +0300

    Add eslint rules, pre-commit eslint hook with fixing flag

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,26 @@ module.exports = {
     browser: true
   },
   rules: {
+    "indent": [
+      "error",
+      2
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "comma-dangle": [
+      "error",
+      "always-multiline"
+    ],
+    "space-before-function-paren": [
+      "error",
+      "never"
+    ],
   },
   overrides: [
     // node files
